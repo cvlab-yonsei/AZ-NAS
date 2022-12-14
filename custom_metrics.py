188674f7736822e12f8059d2d2a6b461f27646b1
junghyuplee <junghyup.lee@yonsei.ac.kr>
2022-12-14 06:40:25 +0000

    rev metrics

--- custom_metrics.py
+++ custom_metrics.py
@@ -22,10 +22,57 @@ def acc_confidence_robustness_metrics(network, inputs, targets):
         prob = torch.nn.functional.softmax(logits, dim=1)
         one_hot_idx = torch.nn.functional.one_hot(targets)
         confidence = (prob[one_hot_idx==1].sum()) / inputs.size(0) * 100 # in percent
-        
+
         # robustness
         _, noisy_logits = network(inputs + torch.randn_like(inputs)*0.1)
         kl_loss = torch.nn.KLDivLoss(reduction="batchmean")
         robustness = -kl_loss(torch.nn.functional.log_softmax(noisy_logits, dim=1), torch.nn.functional.softmax(logits, dim=1))
         
         return acc.item(), confidence.item(), robustness.item()
+
+def step_sim_metric(network, criterion, inputs, targets):
+    original_dict = deepcopy(network.state_dict())
+    optim_large_step = torch.optim.SGD(network.parameters(), lr=0.025)
+    
+    # single large step
+    network.train()
+    optim_large_step.zero_grad()
+    _, logits = network(inputs)
+    base_loss = criterion(logits, targets)
+    base_loss.backward()
+    optim_large_step.step()
+#     _, logits_large_step = network(inputs)
+    large_step_dict = deepcopy(network.state_dict())
+    
+    # multiple small steps
+    network.load_state_dict(original_dict)
+    optim_small_step = torch.optim.SGD(network.parameters(), lr=0.025/3)
+    for i in range(3):
+        optim_small_step.zero_grad()
+        _, logits = network(inputs)
+        base_loss = criterion(logits, targets)
+        base_loss.backward()
+        optim_small_step.step()
+    small_step_dict = deepcopy(network.state_dict())
+    ####
+    scores = []
+    for key in large_step_dict.keys():
+        if ('weight' in key) and (original_dict[key].dim()==4):
+            if (original_dict[key] != large_step_dict[key]).sum():
+                large_step = large_step_dict[key] - original_dict[key]
+                small_step = small_step_dict[key] - original_dict[key]
+                co, ci, kh, kw = large_step.size()
+                large_step = large_step.view(co, -1)
+                small_step = small_step.view(co, -1)
+                score = torch.nn.functional.cosine_similarity(large_step, small_step, dim=1)
+                score = score.mean().item() * 100 # in percent
+                scores.append(score)
+    if len(scores)==0:
+        step_sim = 0
+    else:
+        step_sim = np.mean(scores)
+    
+    # resume
+    network.load_state_dict(original_dict)
+            
+    return step_sim
