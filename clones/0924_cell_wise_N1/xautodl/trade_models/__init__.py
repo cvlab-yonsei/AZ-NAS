d8eaa3c8f0edcf05fcb03227fdfaf5db0122023a
junghyuplee <junghyup.lee@yonsei.ac.kr>
2022-12-21 04:44:31 +0000

    rename test files

--- clones/0924_cell_wise_N1/xautodl/trade_models/__init__.py
+++
@@ -1,4 +0,0 @@
-#####################################################
-# Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2021.03 #
-#####################################################
-from .transformers import get_transformer
--- clones/0924_cell_wise_N1/xautodl/xlayers/__init__.py
+++
@@ -1,7 +0,0 @@
-#####################################################
-# Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2019.01 #
-#####################################################
-# This file is expected to be self-contained, expect
-# for importing from spaces to include search space.
-#####################################################
-from .super_core import *
