4b714cba066858179562a0cd2dc3fcab2423b037
junghyuplee <junghyup.lee@yonsei.ac.kr>
2023-04-09 04:21:24 +0000

    etf17

--- ImageNet_ZiCo/scripts/ETF17_flops450M-150ep-randinput-pop512-iter1e5.sh
+++ ImageNet_ZiCo/scripts/ETF17_flops450M-150ep-randinput-pop512-iter1e5.sh
@@ -19,28 +19,28 @@ echo "SuperConvK3BNRELU(3,8,2,1)SuperResIDWE6K3(8,32,2,8,1)SuperResIDWE6K3(32,48
 SuperResIDWE6K3(48,96,2,48,1)SuperResIDWE6K3(96,128,2,96,1)\
 SuperConvK1BNRELU(128,2048,1,1)" > ${save_dir}/init_plainnet.txt
 
-python evolution_search_etf.py --gpu 0 \
-  --zero_shot_score ETF17 \
-  --search_space SearchSpace/search_space_IDW_fixfc.py \
-  --budget_flops ${budget_flops} \
-  --max_layers ${max_layers} \
-  --batch_size 128 \
-  --input_image_size ${resolution} \
-  --plainnet_struct_txt ${save_dir}/init_plainnet.txt \
-  --num_classes 1000 \
-  --evolution_max_iter ${evolution_max_iter} \
-  --population_size ${population_size} \
-  --save_dir ${save_dir} \
-  --dataset imagenet-1k \
-  --num_worker 16 \
-  --rand_input True \
-  --datapath /dataset/ILSVRC2012/
+# python evolution_search_etf.py --gpu 0 \
+#   --zero_shot_score ETF17 \
+#   --search_space SearchSpace/search_space_IDW_fixfc.py \
+#   --budget_flops ${budget_flops} \
+#   --max_layers ${max_layers} \
+#   --batch_size 128 \
+#   --input_image_size ${resolution} \
+#   --plainnet_struct_txt ${save_dir}/init_plainnet.txt \
+#   --num_classes 1000 \
+#   --evolution_max_iter ${evolution_max_iter} \
+#   --population_size ${population_size} \
+#   --save_dir ${save_dir} \
+#   --dataset imagenet-1k \
+#   --num_worker 16 \
+#   --rand_input True \
+#   --datapath /dataset/ILSVRC2012/
 
-python analyze_model.py \
-  --input_image_size 224 \
-  --num_classes 1000 \
-  --arch Masternet.py:MasterNet \
-  --plainnet_struct_txt ${save_dir}/best_structure.txt
+# python analyze_model.py \
+#   --input_image_size 224 \
+#   --num_classes 1000 \
+#   --arch Masternet.py:MasterNet \
+#   --plainnet_struct_txt ${save_dir}/best_structure.txt
 
 horovodrun -np 8 python train_image_classification.py --dataset imagenet --num_classes 1000 \
   --dist_mode single --workers_per_gpu 8 \
