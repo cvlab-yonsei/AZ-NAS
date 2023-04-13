d4ab5661d7b605f7ac02c41f18b055e9effa0354
junghyuplee <junghyup.lee@yonsei.ac.kr>
2023-04-13 07:37:21 +0000

    albumentations

--- ImageNet_ZiCo/DataLoader/__init__.py
+++ ImageNet_ZiCo/DataLoader/__init__.py
@@ -73,17 +73,17 @@ class Lighting(object):
         self.eigval = eigval
         self.eigvec = eigvec
 
-    def __call__(self, img):
+    def __call__(self, image):
         if self.alphastd == 0:
-            return img
+            return image
 
-        alpha = img.new().resize_(3).normal_(0, self.alphastd)
-        rgb = self.eigvec.type_as(img).clone() \
+        alpha = image.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(image).clone() \
             .mul(alpha.view(1, 3).expand(3, 3)) \
             .mul(self.eigval.view(1, 3).expand(3, 3)) \
             .sum(1).squeeze()
 
-        return img.add(rgb.view(3, 1, 1).expand_as(img))
+        return image.add(rgb.view(3, 1, 1).expand_as(image))
 
 class Transforms:
     def __init__(self, transforms: A.Compose):
