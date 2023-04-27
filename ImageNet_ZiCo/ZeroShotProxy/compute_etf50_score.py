bdb5c56d6d7c4dbfe7d34749484388c6f71bf448
junghyuplee <junghyup.lee@yonsei.ac.kr>
2023-04-27 06:42:08 +0000

    50

--- ImageNet_ZiCo/ZeroShotProxy/compute_etf50_score.py
+++ ImageNet_ZiCo/ZeroShotProxy/compute_etf50_score.py
@@ -165,7 +165,7 @@ def compute_nas_score(model, gpu, trainloader, resolution, batch_size, fp16=Fals
     #################################################
 
     info['expressivity'] = float(fwrd_pca_score) if not np.isnan(fwrd_pca_score) else -np.inf
-    info['fwrd_stability'] = float(fwrd_norm_score) if not np.isnan(fwrd_norm_score) else -np.inf
+    # info['fwrd_stability'] = float(fwrd_norm_score) if not np.isnan(fwrd_norm_score) else -np.inf
     info['bkwd_stability'] = float(bkwd_norm_score) if not np.isnan(bkwd_norm_score) else -np.inf
     info['trainability'] = float(trainability) if not np.isnan(trainability) else -np.inf
     info['capacity'] = float(model.get_model_size())
