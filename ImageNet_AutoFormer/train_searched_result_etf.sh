8c914835f936720735dfee9556149c746a72e6be
junghyuplee <junghyup.lee@yonsei.ac.kr>
2023-06-16 11:57:04 +0900

    Autoformer small / ETF74 450M

--- ImageNet_AutoFormer/train_searched_result_etf.sh
+++ ImageNet_AutoFormer/train_searched_result_etf.sh
@@ -38,26 +38,27 @@
 #  --batch-size 64 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop10000-seed123/TF_TAS-B.yaml' --output_dir './OUTPUT/ETF-pop10000-seed123/TF_TAS-B' --resume './OUTPUT/ETF-pop10000-seed123/TF_TAS-B/checkpoint.pth'
 
 
-##### 4way run
-CUDA_VISIBLE_DEVICES=0,1,2,3, \
-python3 -m torch.distributed.launch --nproc_per_node=4 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
- --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-S.yaml' --init 'xavier_uniform' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev-xavier/TF_TAS-S'
+# ##### 4way run
+# CUDA_VISIBLE_DEVICES=0,1,2,3, \
+# python3 -m torch.distributed.launch --nproc_per_node=4 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
+#  --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-S.yaml' --init 'xavier_uniform' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev-xavier/TF_TAS-S'
 
-CUDA_VISIBLE_DEVICES=4,5,6,7, \
-python3 -m torch.distributed.launch --master_port 7777 --nproc_per_node=4 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
- --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-S.yaml' --init 'trunc_normal' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev-trunc/TF_TAS-S'
+# CUDA_VISIBLE_DEVICES=4,5,6,7, \
+# python3 -m torch.distributed.launch --master_port 7777 --nproc_per_node=4 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
+#  --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-S.yaml' --init 'trunc_normal' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev-trunc/TF_TAS-S'
 
-python3 -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
- --batch-size 128 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-B.yaml' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev/TF_TAS-B'
+# python3 -m torch.distributed.launch --nproc_per_node=8 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
+#  --batch-size 128 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-B.yaml' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev/TF_TAS-B'
 
-CUDA_VISIBLE_DEVICES=0,1,2,3, \
-python3 -m torch.distributed.launch --nproc_per_node=4 --use_env train.py --lr 2.5e-4 --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
- --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-S.yaml' --init 'xavier_uniform' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev-xavier/TF_TAS-S-2.5e-4'
+# CUDA_VISIBLE_DEVICES=0,1,2,3, \
+# python3 -m torch.distributed.launch --nproc_per_node=4 --use_env train.py --lr 2.5e-4 --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
+#  --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev/TF_TAS-S.yaml' --init 'xavier_uniform' --output_dir './OUTPUT/ETF-pop8000-seed0-bs128x8-rev-xavier/TF_TAS-S-2.5e-4'
 
+######
  CUDA_VISIBLE_DEVICES=0,2,4,6, \
 python3 -m torch.distributed.launch --nproc_per_node=4 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
- --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev-xavier/Small.yaml' --init 'trunc_normal' --output_dir './OUTPUT/ETF-pop8000-seed0-bs256x4-rev-xavier/Small'
+ --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-cap-trunc/Small.yaml' --init 'trunc_normal' --output_dir './OUTPUT/ETF-pop8000-seed0-bs256x4-cap-trunc/Small'
 
  CUDA_VISIBLE_DEVICES=1,3,5,7, \
 python3 -m torch.distributed.launch --master_port 7777 --nproc_per_node=4 --use_env train.py --data-path '/dataset/ILSVRC2012' --gp --change_qkv --relative_position \
- --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-rev-xavier/Tiny.yaml' --init 'trunc_normal' --output_dir './OUTPUT/ETF-pop8000-seed0-bs256x4-rev-xavier/Tiny'
+ --batch-size 256 --mode retrain --model_type 'AUTOFORMER' --dist-eval --cfg './experiments/ETF-pop8000-seed0-cap-trunc/Tiny.yaml' --init 'trunc_normal' --output_dir './OUTPUT/ETF-pop8000-seed0-bs256x4-cap-trunc/Tiny'
