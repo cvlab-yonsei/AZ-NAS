603c465f15031a459766fe6305d7254b17f2279b
junghyuplee <junghyup.lee@yonsei.ac.kr>
2023-06-21 10:49:08 +0900

    revise train scripts

--- ImageNet_MBV2/scripts/train_ETFxx_flops600M-480ep-topkmut-searchbs64.sh
+++ ImageNet_MBV2/scripts/train_ETFxx_flops600M-480ep-topkmut-searchbs64.sh
@@ -39,7 +39,8 @@ python analyze_model.py \
   --arch Masternet.py:MasterNet \
   --plainnet_struct_txt ${save_dir}/best_structure.txt
 
-horovodrun -np 8 python ts_train_image_classification.py --dataset imagenet --num_classes 1000 \
+CUDA_VISIBLE_DEVICES=1,3,5,7, \
+horovodrun -np 4 python ts_train_image_classification.py --dataset imagenet --num_classes 1000 \
   --dist_mode single --workers_per_gpu ${num_workers} \
   --input_image_size ${resolution} --epochs ${epochs} --warmup 5 \
   --optimizer sgd --bn_momentum 0.01 --wd 4e-5 --nesterov --weight_init ${init} \
@@ -56,6 +57,27 @@ horovodrun -np 8 python ts_train_image_classification.py --dataset imagenet --nu
   --ts_proj_no_bn \
   --use_se \
   --target_downsample_ratio 16 \
-  --batch_size_per_gpu 64 --save_dir ${save_dir}/ts_training_epochs${epochs}_init-${init} \
-  --world-size 8 \
+  --batch_size_per_gpu 128 --save_dir ${save_dir}/ts_training_epochs${epochs}_init-${init} \
+  --world-size 4 \
   --dist_mode horovod\
+
+# horovodrun -np 8 python ts_train_image_classification.py --dataset imagenet --num_classes 1000 \
+#   --dist_mode single --workers_per_gpu ${num_workers} \
+#   --input_image_size ${resolution} --epochs ${epochs} --warmup 5 \
+#   --optimizer sgd --bn_momentum 0.01 --wd 4e-5 --nesterov --weight_init ${init} \
+#   --label_smoothing --random_erase --mixup --auto_augment \
+#   --lr_per_256 0.1 --target_lr_per_256 0.0 --lr_mode cosine \
+#   --arch Masternet.py:MasterNet \
+#   --plainnet_struct_txt ${save_dir}/best_structure.txt \
+#   --teacher_arch geffnet_tf_efficientnet_b3_ns \
+#   --teacher_pretrained \
+#   --teacher_input_image_size 320 \
+#   --teacher_feature_weight 1.0 \
+#   --teacher_logit_weight 1.0 \
+#   --ts_proj_no_relu \
+#   --ts_proj_no_bn \
+#   --use_se \
+#   --target_downsample_ratio 16 \
+#   --batch_size_per_gpu 64 --save_dir ${save_dir}/ts_training_epochs${epochs}_init-${init} \
+#   --world-size 8 \
+#   --dist_mode horovod\
